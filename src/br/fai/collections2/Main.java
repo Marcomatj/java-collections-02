637ab22570f0882eff51871d027c9b564c0ae45c
Marco Antonio <marcoantonio_teodoro@hotmail.com>
2020-03-11 19:42:07 -0300

    Primeiro commit

---
+++ src/br/fai/collections2/Main.java
@@ -0,0 +1,19 @@
+package br.fai.collections2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Main app = new Main();
+		app.start();
+		
+
+	}
+
+	private void start() {
+		
+		
+		
+	}
+
+}
